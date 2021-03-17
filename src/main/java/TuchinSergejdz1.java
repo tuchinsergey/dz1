fb9e97be47c13f88821a3452c38eed825a74cb7c
sergey <tuchinsergey29@yandex.ru>
2021-03-17 20:54:32 +0300

    Initial commit

---
+++ src/main/java/TuchinSergejdz1.java
@@ -0,0 +1,25 @@
+public class homework1 {
+    static double task3(double a, double b, double c, double d) {
+        return a * (b + (c / d));
+    }
+    static boolean task4(int a, int b) {
+        int sum = a + b;
+        return sum >= 10 && sum <= 20;     }
+    static boolean task6(int a)
+    { if(a<0) return true;
+    else return false;
+    }
+    static void task7(String name)
+    { System.out.println("Привет"+name);
+    }
+    public static void main(String[] args) {
+        System.out.println(task3(2 ,3,0,7));
+        System.out.println(task4(2,7));
+        System.out.println(task6(-5));
+        task7(  " Сергей");
+    }
+
+}
+
+
+
---
+++ src/main/java/myfirstclass.java
@@ -0,0 +1,15 @@
+public class myfirstclass {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        // переменные типов данных
+        byte byteValue =15;
+        short shortValue =12000;
+        long longValue=125000L;
+        float floatValue =12.23f;
+        double doubleValue =-123.123;
+        char charValue='\u2242';
+        boolean booleanValue =true;
+        System.out.println(1+3);
+
+} }
+
