69132e3787a5eb4717d7a50335ff1a53afc438e1
sergey <tuchinsergey29@yandex.ru>
2021-03-24 16:18:23 +0300

    HomeWork3

--- src/main/java/homework3/HomeWork3.java
+++ src/main/java/homework3/HomeWork3.java
@@ -55,7 +55,6 @@ public class HomeWork3 {
                 }
             }
     }
-
             public static void main(String[] args) {
         System.out.println("Вы в игре угадай число"+"У вас есть 3 попытки");
         run();
