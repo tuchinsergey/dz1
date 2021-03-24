706e41a501d0c3f935505c8bfcc838899d2b1695
sergey <tuchinsergey29@yandex.ru>
2021-03-24 14:53:27 +0300

    Homework2

---
+++ src/main/java/homework3/HomeWork3.java
@@ -0,0 +1,17 @@
+package homework3;
+
+public class HomeWork3 {
+
+    private static int getRandomDigit() {
+        return(int) (Math.random()*10);
+    }
+
+    private static void run(){
+        int guess=getRandomDigit();
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
