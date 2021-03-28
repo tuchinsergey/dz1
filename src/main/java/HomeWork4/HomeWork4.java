bdee08b464422079678f2f973dc9399655473931
sergey <tuchinsergey29@yandex.ru>
2021-03-28 19:16:26 +0300

    HomeWork3

--- src/main/java/HomeWork4/HomeWork4.java
+++ src/main/java/HomeWork4/HomeWork4.java
@@ -1,6 +1,5 @@
 package HomeWork4;
-import javax.swing.table.DefaultTableCellRenderer;
-import java.util.ConcurrentModificationException;
+import java.util.concurrent.ConcurrentMap;
 import java.util.Scanner;
 
 public class HomeWork4 {
@@ -41,13 +40,88 @@ private static void printMap() {
                     int y = Integer.parseInt(args[1]);
                     x--;
                     y--;
-                    if ()
+                }
+                while (true) {
+                    humanTurn();
+                    printMap();
+                    if (checkWin(DOT_X)) {
+                        System.out.println("Победил человек");
+                        break;
+                    }
+                    if (isMapFull()) {
+                        System.out.println("Ничья");
+                        break;
+                    }
+                    aiTurn();
+                    printMap();
+                    if (checkWin(DOT_O)) {
+                        System.out.println("Победил Искуственный Интеллект");
+                        break;
+                    }
+                    if (isMapFull()) {
+                        System.out.println("Ничья");
+                        break;
+                    }
+                }
+                System.out.println("Игра закончена");
             }
-
+            public static boolean isMapFull() {
+                for (int i = 0; i < SIZE; i++) {
+                    for (int j = 0; j < SIZE; j++) {
+                        if (map[i][j] == DOT_EMPTY) return false;
+                    }
+                }
+                return true;
             }
-
-
+            public static void aiTurn() {
+                int x, y;
+                do {
+                    x = rand.nextInt(SIZE);
+                    y = rand.nextInt(SIZE);
+                } while (!isCellValid(x, y));
+                System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y + 1));
+                map[y][x] = DOT_O;
+            }
+            public static void humanTurn() {
+                int x, y;
+                do {
+                    System.out.println("Введите координаты в формате X Y");
+                    x = sc.nextInt() - 1;
+                    y = sc.nextInt() - 1;
+                } while (!isCellValid(x, y)); // while(isCellValid(x, y) == false)
+                map[y][x] = DOT_X;
+            }
+            public static boolean isCellValid(int x, int y) {
+                if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
+                if (map[y][x] == DOT_EMPTY) return true;
+                return false;
+            }
+            public static void initMap() {
+                map = new char[SIZE][SIZE];
+                for (int i = 0; i < SIZE; i++) {
+                    for (int j = 0; j < SIZE; j++) {
+                        map[i][j] = DOT_EMPTY;
+                    }
+                }
+            }
+            public static void printMap() {
+                for (int i = 0; i <= SIZE; i++) {
+                    System.out.print(i + " ");
+                }
+                System.out.println();
+                for (int i = 0; i < SIZE; i++) {
+                    System.out.print((i + 1) + " ");
+                    for (int j = 0; j < SIZE; j++) {
+                        System.out.print(map[i][j] + " ");
+                    }
+                    System.out.println();
+                }
+                System.out.println();
+            }
         }
-    }
 
-    }
+
+
+
+
+
