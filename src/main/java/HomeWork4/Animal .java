fdc8518071a74776f9acc07c545e36063e9a0e98
sergey <tuchinsergey29@yandex.ru>
2021-04-04 11:02:54 +0300

    HomeWork3

---
+++ src/main/java/HomeWork4/Animal .java
@@ -0,0 +1,27 @@
+package HomeWork4;
+
+public class Animal  {
+    public Animal() {
+    }
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public void animalInfo() {
+        System.out.println("Животное: " + name);
+    }
+    public Cat(String name, String color) {
+        this.name = name;
+    }
+    public class AnimalsApp {
+        public static void main(String[] args) {
+            Animal animal = new Animal("Дружок");
+            Cat cat = new Cat("Барсик",);
+            animal.animalInfo();
+            cat.animalInfo();
+            cat.catInfo();
+        }
+
+
+    }
